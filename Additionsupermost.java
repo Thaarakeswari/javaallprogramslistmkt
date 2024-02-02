fdd070c8658f06be11696793d5ef7f6ce8a15660
harirepo <harishankar.8569@gmail.com>
2024-02-02 19:13:33 +0530

    Adding 1st java program

---
+++ Additionsupermost.java
@@ -0,0 +1,13 @@
+package classwork;
+
+public class Additionsupermost extends Object{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Additionsupermost  a1=new Additionsupermost();
+		System.out.println(a1.getClass());
+		
+
+	}
+
+}
